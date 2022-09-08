ca16e8b3071a732f676b5480dcd4fc68787276c3
soalemusharafa <soalemusharafa25@gmail.com>
2022-09-08 00:58:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
+ * Description: main - Prints out a string to stdout.
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ * Return: 0 if success.
+ */
 
 int main(void)
 
