991220b06cc7238ebaddc513a18019d04cb5601b
soalemusharafa <soalemusharafa25@gmail.com>
2022-09-14 14:40:32 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,43 +1,36 @@
 #include "main.h"
 
 /**
-
- * print_alphabet - Prints the alphabet in lowercase
- * Return: Always 0 (Success)
+ * main - Prints _putchar
  *
+ * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 
 {
 
 
 
-	char alpha;
+	_putchar('_');
 
+	_putchar('p');
 
+	_putchar('u');
 
+	_putchar('t');
 
+	_putchar('c');
 
+	_putchar('h');
 
+	_putchar('a');
 
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-
-
-
-	{
-
-
-
-		_putchar(alpha);
-
-
-
-	}
-
-
+	_putchar('r');
 
 	_putchar('\n');
 
 
+
+	return (0);
 
 }
