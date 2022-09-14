6b4a7aa20851fc5c5985e7d3a7801d68ee30f148
soalemusharafa <soalemusharafa25@gmail.com>
2022-09-14 16:39:03 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,50 +8,26 @@ void print_alphabet_x10(void)
 
 {
 
-
-
 	int alpha;
-
-
 
 	int alpha2;
 
 
 
-
-
-
-
 	for (alpha = 0; alpha <= 9; alpha++)
 
-
-
 	{
-
-
-
+	
 		for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
-
-
-
+			
 		{
-
-
-
+	
 			_putchar(alpha2);
-
-
-
+	
 		}
-
-
 
 		_putchar('\n');
 
-
-
 	}
-
-
 
 }
